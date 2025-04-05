4ade4c1f471d84df04d9fe88b6b43542d743005f
Suntanqing FU <fusuntanqing@gmail.com>
2025-04-05 17:46:31 +0100

    deleted collections using

--- DeliveredOrderManager.java
+++ DeliveredOrderManager.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class DeliveredOrderManager {
-    private static final List<Order> deliveredOrders = Collections.synchronizedList(new ArrayList<>());
+    private static final List<Order> deliveredOrders = new ArrayList<>();
 
     public static void addDeliveredOrder(Order order) {
         if (order != null) {
-            deliveredOrders.add(order);
+            synchronized (deliveredOrders) {
+                deliveredOrders.add(order);
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Order cannot be null");
         }
     }
 
     public static List<Order> getDeliveredOrders() {
-        return new ArrayList<>(deliveredOrders);
+        synchronized (deliveredOrders) {
+            return new ArrayList<>(deliveredOrders);
+        }
     }
 }
