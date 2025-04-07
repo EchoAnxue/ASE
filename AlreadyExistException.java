e98923be53b453d04f98074ec5c757dfdf86f339
Bilawal Hassan <128214198+BilawalHassanF19csc17@users.noreply.github.com>
2025-04-07 02:44:30 +0100

    initial commit

--- AlreadyExistException.java
+++ AlreadyExistException.java
@@ -1,10 +1,13 @@
-// This is the custom exception class for handling the case when a requested item is already exist.
-// @author <Suntanqing FU> <sf4009@hw.ac.uk>
-// @version 0.01
-// @since 2025-02-09
+// This is the custom exception class for handling the case when a requested item already exists.
+// Author: Suntanqing FU <sf4009@hw.ac.uk> , Bilawal Hassan <bh3006@hw.ac.uk>
+// Version: 0.01
+// Since: 2025-02-09
+
 public class AlreadyExistException extends Exception {
 
-    public AlreadyExistException (String message) {
-        super(message+" has already exist. Please check.");
+    private static final long serialVersionUID = 1L;
+
+    public AlreadyExistException(String message) {
+        super(message + " already exists. Please check.");
     }
 }
